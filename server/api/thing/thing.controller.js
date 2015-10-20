aa23f92409ec2125db0ba33a966c1be3603a7bce
Sergio Morcuende <sergio.morcuende@bq.com>
2015-10-20 15:08:59 +0200

    Disable mongoose

--- server/api/thing/thing.controller.js
+++ server/api/thing/thing.controller.js
@@ -7,39 +7,46 @@
 'use strict';
 
 var _ = require('lodash');
-var Thing = require('./thing.model');
+
+var fs = require('fs');
+var Thing = JSON.parse(fs.readFileSync('./server/api/thing/thing.seed.json', 'utf8'));
+
+// var Thing = require('./thing.model');
 
 // Get list of things
 exports.index = function(req, res) {
-  Thing.find(function(err, things) {
-    if (err) {
-      return handleError(res, err);
-    }
-    return res.status(200).json(things);
-  });
+  // Thing.find(function(err, things) {
+  //   if (err) {
+  //     return handleError(res, err);
+  //   }
+  //   return res.status(200).json(things);
+  // });
+  return res.status(200).json(Thing);
 };
 
 exports.random = function(req, res) {
-  Thing.find(function(err, things) {
-    if (err) {
-      return handleError(res, err);
-    }
-    var thing = things[Math.floor(0 + Math.random() * things.length)];
-    return res.status(200).json(thing);
-  });
+  // Thing.find(function(err, things) {
+  //   if (err) {
+  //     return handleError(res, err);
+  //   }
+  //   var thing = things[Math.floor(0 + Math.random() * things.length)];
+  //   return res.status(200).json(thing);
+  // });
+  var randomThing = Thing[Math.floor(0 + Math.random() * Thing.length)];
+  return res.status(200).json(randomThing);
 };
 
 // Get a single thing
 exports.show = function(req, res) {
-  Thing.findById(req.params.id, function(err, thing) {
-    if (err) {
-      return handleError(res, err);
-    }
-    if (!thing) {
-      return res.status(404).send('Not Found');
-    }
-    return res.json(thing);
-  });
+  // Thing.findById(req.params.id, function(err, thing) {
+  //   if (err) {
+  //     return handleError(res, err);
+  //   }
+  //   if (!thing) {
+  //     return res.status(404).send('Not Found');
+  //   }
+  //   return res.json(thing);
+  // });
 };
 
 function handleError(res, err) {
--- server/app.js
+++ server/app.js
@@ -8,18 +8,18 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 var config = require('./config/environment');
 
 // Connect to database
-mongoose.connect(config.mongo.uri, config.mongo.options);
-mongoose.connection.on('error', function(err) {
-	console.error('MongoDB connection error: ' + err);
-	process.exit(-1);
-	}
-);
+// mongoose.connect(config.mongo.uri, config.mongo.options);
+// mongoose.connection.on('error', function(err) {
+// 	console.error('MongoDB connection error: ' + err);
+// 	process.exit(-1);
+// 	}
+// );
 // Populate DB with sample data
-if(config.seedDB) { require('./config/seed'); }
+// if(config.seedDB) { require('./config/seed'); }
 
 // Setup server
 var app = express();
@@ -28,7 +28,7 @@ require('./config/express')(app);
 require('./routes')(app);
 
 // Start server
-server.listen(config.port, config.ip, function () {
+server.listen(config.port, config.ip, function() {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
